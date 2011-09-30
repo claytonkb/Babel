a75daf7fe35fe299d42b52a65dfeb56625cd379a
Clayton Bauman <claytonkb@gmail.com>
2011-09-30 15:21:32 -0700

    Plumbing in support for insha() and inskha()

--- src/hash.c
+++ src/hash.c
@@ -4,7 +4,19 @@
 #include "babel.h"
 #include "hash.h"
 
+// make hash-entry
+mword *mk_hash_entry(mword *val){
+}
 
+// make hash-entry with key
+mword *mk_hash_key_entry(mword *val, mword *key){
+}
+
+void _insha(mword *hash_table, mword *val){
+}
+
+void _inskha(mword *hash_table, mword *val, mword *key){
+}
 
 // Clayton Bauman 2011
 
--- src/hash.h
+++ src/hash.h
@@ -4,7 +4,10 @@
 #ifndef HASH_H
 #define HASH_H
 
-
+mword *mk_hash_entry(mword *val);
+mword *mk_hash_key_entry(mword *val, mword *key);
+void _insha(mword *hash_table, mword *val);
+void _inskha(mword *hash_table, mword *val, mword *key);
 
 #endif //HASH_H
 
