7199f030ad91d5a1e9957df15faa6326ecb691b2
Clayton Bauman <claytonkb@gmail.com>
2011-10-07 16:52:35 -0700

    More work on insha()

--- src/hash.c
+++ src/hash.c
@@ -3,16 +3,40 @@
 
 #include "babel.h"
 #include "hash.h"
+#include "list.h"
+#include "except.h"
+
+//void push_alloc(mword *operand, mword alloc_type){
 
 // make hash-entry
-mword *mk_hash_entry(mword *val){
+mword *new_hash_entry(mword *val, mword *hash){
+
+    mword *temp_consA = new_cons();
+    mword *temp_consB = new_cons();
+
+    cons(temp_consA,  hash,  nil);
+    cons(temp_consB,  val,   temp_consA);
+
+    return temp_consB;
+
 }
 
 // make hash-entry with key
-mword *mk_hash_key_entry(mword *val, mword *key){
+mword *new_hash_key_entry(mword *val, mword *hash, mword *key){
 }
 
 void _insha(mword *hash_table, mword *val){
+
+    if(hash_table == (mword*)nil){ //nil
+    }
+    else if( is_inte(hash_table) && (size(hash_table) == 2) ){ //list
+    }
+    else if( is_inte(hash_table) && (size(hash_table) == HASH_LEVEL_SIZE) ){ //list
+    }
+    else{
+        except("_insha: Not a hash", __FILE__, __LINE__);
+    }
+
 }
 
 void _inskha(mword *hash_table, mword *val, mword *key){
--- src/hash.h
+++ src/hash.h
@@ -4,8 +4,11 @@
 #ifndef HASH_H
 #define HASH_H
 
-mword *mk_hash_entry(mword *val);
-mword *mk_hash_key_entry(mword *val, mword *key);
+#define BITS_PER_HASH_LEVEL (4)
+#define HASH_LEVEL_SIZE     (1<<BITS_PER_HASH_LEVEL)
+
+mword *new_hash_entry(mword *val, mword *hash);
+mword *new_hash_key_entry(mword *val, mword *hash, mword *key);
 void _insha(mword *hash_table, mword *val);
 void _inskha(mword *hash_table, mword *val, mword *key);
 
