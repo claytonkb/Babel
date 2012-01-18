ad07679b0947566a60dbd104c5a5a40b0b489f6f
Clayton Bauman <claytonkb@gmail.com>
2012-01-17 22:42:16 -0800

    Working on del_tree

--- src/alloc.c
+++ src/alloc.c
@@ -40,54 +40,35 @@ void _del(mword *tree){
 
     mword offset = 0;
 
-    del_tree(tree, arr_list, offset);
+    del_tree(tree, arr_list, &offset);
 
 }
 
-void del_tree(mword *tree, mword *arr_list, mword offset){
+void del_tree(mword *tree, mword *arr_list, mword *offset){
 
-//    int i;
-//
-//    if( s((mword*)(tree+offset)) & (MWORD_SIZE-1) ){ //Already dumped
-//        return get_abs_offset(LUT_rel, LUT_abs, offset*MWORD_SIZE);
-//    }
-//
-//    int num_elem = size(tree+offset);
-//    
-//    s(tree+offset) |= 0x1; //Mark dumped
-//
-//    mword *new_arr;
-//
-//    if(is_inte(tree+offset)){
-//        new_arr = _newin_blank(num_elem);
-//
-//        c(LUT_rel,*LUT_offset) = offset*MWORD_SIZE;
-//        c(LUT_abs,*LUT_offset) = (mword)new_arr;
-//        *LUT_offset = *LUT_offset+1;        
-//
-//        for(i=0; i<num_elem; i++){
-//            c(new_arr,i) = (mword)load_tree(
-//                tree,
-//                c(tree,offset+i),
-//                LUT_abs, 
-//                LUT_rel, 
-//                LUT_offset);
-//        }
-//
-//    }
-//    else{
-//        new_arr = _newlf(num_elem);
-//
-//        c(LUT_rel,*LUT_offset) = offset*MWORD_SIZE;
-//        c(LUT_abs,*LUT_offset) = (mword)new_arr;
-//        *LUT_offset = *LUT_offset+1;
-//
-//        for(i=0; i<num_elem; i++){
-//            c(new_arr,i) = c(tree,offset+i);
-//        }
-//    }
-//
-//    return new_arr;
+    offset /= MWORD_SIZE;
+
+    int i;
+
+    if( s(tree) & (MWORD_SIZE-1) ){ //Already dumped
+        return;
+    }
+
+    int num_elem = size(tree);
+    
+    s(tree) |= 0x1; //Mark dumped
+
+    c(arr_list,*offset) = c(tree,i);
+
+    if(is_inte(tree)){
+        for(i=0; i<num_elem; i++){
+            c(arr_list,*offset) = c(tree,i);
+            *offset = *offset+1;
+            del_tree((mword*)c(tree,i),arr_list,offset);
+        }
+    }
+
+    return new_arr;
 
 }
 
--- src/alloc.h
+++ src/alloc.h
@@ -8,7 +8,7 @@ void free_op(void);
 void _free_op(mword *mem);
 void del(void);
 void _del(mword *tree);
-void del_tree(mword *tree, mword *arr_list, mword offset);
+void del_tree(mword *tree, mword *arr_list, mword *offset);
 mword check_address(mword *arr_list, mword *address);
 
 #endif //ALLOC_H
