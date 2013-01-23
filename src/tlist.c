9729b344935833816850306e9426363032e036a2
Clayton Bauman <claytonkb@gmail.com>
2013-01-22 23:01:16 -0800

    Added tlist.[ch]

---
+++ src/tlist.c
@@ -0,0 +1,10 @@
+// tlist.c
+//
+
+#include "tlist.h"
+
+//tag-checking (equal, etc.)
+//new tagged-list
+
+// Clayton Bauman 2013
+
---
+++ src/tlist.h
@@ -0,0 +1,12 @@
+// tlist.h
+// 
+
+#ifndef TLIST_H
+#define TLIST_H
+
+#include "babel.h"
+
+#endif //HASH_H
+
+// Clayton Bauman 2013
+
