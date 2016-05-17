ba3218827a11c482c2be4188097006a072d36ae0
Clayton Bauman <claytonkb@gmail.com>
2016-05-17 14:01:23 -0700

    Repo was missing null.sp.*

---
+++ src/null.sp.h
@@ -0,0 +1,15 @@
+// null.sp.h
+//
+
+#ifndef NULL_SP_H
+#define NULL_SP_H
+
+#include "babel.h"
+
+#define NULL_BVM_SIZE 410
+
+extern const mword null_bvm[NULL_BVM_SIZE];
+
+#endif //NULL_SP_H
+
+// Clayton Bauman 2016
